410752c9b9ececa3ed98b6a78e79d76288446fbb
Kris Waithe <KBWaithe@gmail.com>
2020-08-21 18:41:38 -0400

    Populated ts and js file

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -4,8 +4,12 @@ let Game = (function () {
     let canvas = document.getElementsByTagName('canvas')[0];
     let stage;
     let assets;
-    let exampleLabel;
-    let exampleButton;
+    let diceOneLabel;
+    let diceTwoLabel;
+    let diceResultLabel;
+    let diceDesignOne;
+    let diceDesignTwo;
+    let rollButton;
     let assetManifest = [
         { id: "1", src: "./Assets/images/1.png" },
         { id: "2", src: "./Assets/images/2.png" },
@@ -13,19 +17,19 @@ let Game = (function () {
         { id: "4", src: "./Assets/images/4.png" },
         { id: "5", src: "./Assets/images/5.png" },
         { id: "6", src: "./Assets/images/6.png" },
-        { id: "backButton", src: "./Assets/images/startButton.png" },
-        { id: "background", src: "./Assets/images/background.png" },
-        { id: "blank", src: "./Assets/images/blank.png" },
-        { id: "button", src: "./Assets/images/button.png" },
-        { id: "nextButton", src: "./Assets/images/nextButton.png" },
-        { id: "placeholder", src: "./Assets/images/placeholder.png" },
-        { id: "resetButton", src: "./Assets/images/resetButton.png" },
+        //{ id: "backButton", src: "./Assets/images/startButton.png" },
+        //{ id: "background", src: "./Assets/images/background.png" },
+        //{ id: "blank", src: "./Assets/images/blank.png" },
+        //{ id: "button", src: "./Assets/images/button.png" },
+        //{ id: "nextButton", src: "./Assets/images/nextButton.png" },
+        //{ id: "placeholder", src: "./Assets/images/placeholder.png" },
+        //{ id: "resetButton", src: "./Assets/images/resetButton.png" },
         { id: "rollButton", src: "./Assets/images/rollButton.png" },
-        { id: "startButton", src: "./Assets/images/startButton.png" },
-        { id: "startOverButton", src: "./Assets/images/startOverButton.png" }
+        //{ id: "startButton", src: "./Assets/images/startButton.png" },
+        //{ id: "startOverButton", src: "./Assets/images/startOverButton.png" }
     ];
     function Preload() {
-        console.log(`%c Preload Function`, "color: grey; font-size: 14px; font-weight: bold;");
+        console.log(`%c Preload Function`, "color: red; font-size: 14px; font-weight: bold;");
         assets = new createjs.LoadQueue(); // asset container 
         assets.installPlugin(createjs.Sound); // supports sound preloading
         assets.loadManifest(assetManifest);
@@ -36,7 +40,7 @@ let Game = (function () {
      * It sets the framerate to 60 FPS and sets up the main Game Loop (Update)
      */
     function Start() {
-        console.log(`%c Start Function`, "color: grey; font-size: 14px; font-weight: bold;");
+        console.log(`%c Start Function`, "color: green; font-size: 14px; font-weight: bold;");
         stage = new createjs.Stage(canvas);
         createjs.Ticker.framerate = Config.Game.FPS;
         createjs.Ticker.on('tick', Update);
@@ -51,18 +55,43 @@ let Game = (function () {
     function Update() {
         stage.update();
     }
-    /**
+    /*
      * This is the main function of the Game (where all the fun happens)
      *
      */
-    function Main() {
-        console.log(`%c Main Function`, "color: grey; font-size: 14px; font-weight: bold;");
-        exampleLabel = new UIObjects.Label("An Example Label", "40px", "Consolas", "#000000", Config.Game.CENTER_X, Config.Game.CENTER_Y, true);
-        stage.addChild(exampleLabel);
-        exampleButton = new UIObjects.Button("button", Config.Game.CENTER_X, Config.Game.CENTER_Y + 100, true);
-        stage.addChild(exampleButton);
-        exampleButton.on("click", () => {
-            console.log("example button clicked");
+    function Main() 
+    {
+        console.log(`%c Main Function`, "color: blue; font-size: 14px; font-weight: bold;");
+
+        /* Button Functionality first */
+        rollButton = new UIObjects.Button("rollButton", Config.Game.CENTER_X, Config.Game.CENTER_Y + 100, true);
+        stage.addChild(rollButton);
+
+        rollButton.on("click", ()=>{
+            console.log("The dice have been cast.");
+            let ranNumGenOne = generateRandomNumber();
+            let ranNumGenTwo = generateRandomNumber();
+            let diceResults = ranNumGenOne + ranNumGenTwo;
+
+            /* Clear the labels, dice designs and results in order to reroll */
+            stage.removeChild(diceOneLabel, diceTwoLabel, diceDesignOne, diceDesignTwo, diceResultLabel)
+            
+            /* Linking first and second RNG functions to respective labels then appending the labels to the stage */
+            diceOneLabel = new UIObjects.Label(ranNumGenOne.toString(), "40px", "Consolas", "#000000", Config.Game.CENTER_X, Config.Game.CENTER_Y + 100, true);
+            stage.addChild(diceOneLabel);
+            
+            diceTwoLabel = new UIObjects.Label(ranNumGenTwo.toString(), "40px", "Consolas", "#000000", Config.Game.CENTER_X, Config.Game.CENTER_Y + 100, true);
+            stage.addChild(diceTwoLabel);
+
+            /* Linking results to the result label */
+            diceResultLabel = new UIObjects.Label(diceResults.toString (), "35px", "Consolas", "#000000", Config.Game.CENTER_X, Config.Game.CENTER_Y + 90, true);
+            
+            /* Linking the designs for the dice to the dice object */
+            diceDesignOne = new Core.GameObject(ranNumGenOne.toString(), Config.Game.CENTER_X - 190, Config.Game.CENTER_Y - 110, true);
+            stage.addChild(diceDesignOne);
+            
+            diceDesignTwo = new Core.GameObject(ranNumGenTwo.toString(), Config.Game.CENTER_X - 190, Config.Game.CENTER_Y - 110, true);
+            stage.addChild(diceDesignTwo);
         });
     }
     window.addEventListener('load', Preload);
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -6,10 +6,19 @@ let Game = (function(){
     
     let assets: createjs.LoadQueue;
 
-    let exampleLabel: UIObjects.Label;
-    let exampleButton: UIObjects.Button;
-    
+    /* Labels */
+    let diceOneLabel: UIObjects.Label;
+    let diceTwoLabel: UIObjects.Label;
+    let diceResultLabel: UIObjects.Label;
 
+    /* Dice designs */
+    let diceDesignOne: Core.GameObject;
+    let diceDesignTwo: Core.GameObject;
+    
+    /* Buttons */
+    let rollButton: UIObjects.Button;
+       
+    /* Instead of deleting the other assets, I commented them out just in case I could utilize them later on */
     let assetManifest = 
     [
         {id:"1", src:"./Assets/images/1.png"},
@@ -18,34 +27,34 @@ let Game = (function(){
         {id:"4", src:"./Assets/images/4.png"},
         {id:"5", src:"./Assets/images/5.png"},
         {id:"6", src:"./Assets/images/6.png"},
-        {id:"backButton", src:"./Assets/images/startButton.png"},
-        {id:"background", src:"./Assets/images/background.png"},
-        {id:"blank", src:"./Assets/images/blank.png"},
-        {id:"button", src:"./Assets/images/button.png"},
-        {id:"nextButton", src:"./Assets/images/nextButton.png"},
-        {id:"placeholder", src:"./Assets/images/placeholder.png"},
-        {id:"resetButton", src:"./Assets/images/resetButton.png"},
+        //{id:"backButton", src:"./Assets/images/startButton.png"},
+        //{id:"background", src:"./Assets/images/background.png"},
+        //{id:"blank", src:"./Assets/images/blank.png"},
+        //{id:"button", src:"./Assets/images/button.png"},
+        //{id:"nextButton", src:"./Assets/images/nextButton.png"},
+        //{id:"placeholder", src:"./Assets/images/placeholder.png"},
+        //{id:"resetButton", src:"./Assets/images/resetButton.png"},
         {id:"rollButton", src:"./Assets/images/rollButton.png"},
-        {id:"startButton", src:"./Assets/images/startButton.png"},
-        {id:"startOverButton", src:"./Assets/images/startOverButton.png"}
+        //{id:"startButton", src:"./Assets/images/startButton.png"},
+        //{id:"startOverButton", src:"./Assets/images/startOverButton.png"}
     ];
 
     function Preload():void
     {
-        console.log(`%c Preload Function`, "color: grey; font-size: 14px; font-weight: bold;");
+        console.log(`%c Preload Function`, "color: green; font-size: 14px; font-weight: bold;");
         assets = new createjs.LoadQueue(); // asset container 
         assets.installPlugin(createjs.Sound); // supports sound preloading
         assets.loadManifest(assetManifest);
         assets.on("complete", Start);
     }
 
-    /**
+    /*
      * This method initializes the CreateJS (EaselJS) Library
      * It sets the framerate to 60 FPS and sets up the main Game Loop (Update)
      */
     function Start():void
     {
-        console.log(`%c Start Function`, "color: grey; font-size: 14px; font-weight: bold;");
+        console.log(`%c Start Function`, "color: green; font-size: 14px; font-weight: bold;");
         stage = new createjs.Stage(canvas);
         createjs.Ticker.framerate = Config.Game.FPS;
         createjs.Ticker.on('tick', Update);
@@ -58,29 +67,56 @@ let Game = (function(){
 
     /**
      * This function is triggered every frame (16ms)
-     * The stage is then erased and redrawn 
+     * The stage is then erased and redrawn 60x per second
      */
     function Update():void
     {
         stage.update();
     }
 
+    /* RNG functionality for the dice*/
+    function generateRandomNumber()
+    {
+        return Math.floor(Math.random()*(6)+1);
+    }
+
     /**
-     * This is the main function of the Game (where all the fun happens)
+     * Game Functionality
      *
      */
     function Main():void
     {
-        console.log(`%c Main Function`, "color: grey; font-size: 14px; font-weight: bold;");
+        console.log(`%c Main Function`, "color: green; font-size: 14px; font-weight: bold;");
 
-        exampleLabel = new UIObjects.Label("An Example Label", "40px", "Consolas", "#000000", Config.Game.CENTER_X, Config.Game.CENTER_Y, true);
-        stage.addChild(exampleLabel);
+        /* Button Functionality first */
+        rollButton = new UIObjects.Button("rollButton", Config.Game.CENTER_X, Config.Game.CENTER_Y + 100, true);
+        stage.addChild(rollButton);
 
-        exampleButton = new UIObjects.Button("button", Config.Game.CENTER_X, Config.Game.CENTER_Y + 100, true);
-        stage.addChild(exampleButton);
+        rollButton.on("click", ()=>{
+            console.log("The dice have been cast.");
+            let ranNumGenOne = generateRandomNumber();
+            let ranNumGenTwo = generateRandomNumber();
+            let diceResults = ranNumGenOne + ranNumGenTwo;
 
-        exampleButton.on("click", ()=>{
-            console.log("example button clicked");
+            /* Clear the labels, dice designs and results in order to reroll */
+            stage.removeChild(diceOneLabel, diceTwoLabel, diceDesignOne, diceDesignTwo, diceResultLabel)
+            
+            /* Linking first and second RNG functions to respective labels then appending the labels to the stage */
+            diceOneLabel = new UIObjects.Label(ranNumGenOne.toString(), "40px", "Consolas", "#000000", Config.Game.CENTER_X - 190, Config.Game.CENTER_Y + 100, true);
+            stage.addChild(diceOneLabel);
+            
+            diceTwoLabel = new UIObjects.Label(ranNumGenTwo.toString(), "40px", "Consolas", "#000000", Config.Game.CENTER_X - 190, Config.Game.CENTER_Y + 100, true);
+            stage.addChild(diceTwoLabel);
+
+            /* Linking results to the result label */
+            diceResultLabel = new UIObjects.Label(diceResults.toString (), "35px", "Consolas", "#000000", Config.Game.CENTER_X, Config.Game.CENTER_Y + 90, true);
+            
+            /* Linking the designs for the dice to the dice object */
+            diceDesignOne = new Core.GameObject(ranNumGenOne.toString(), Config.Game.CENTER_X - 190, Config.Game.CENTER_Y - 110, true);
+            stage.addChild(diceDesignOne);
+            
+            diceDesignTwo = new Core.GameObject(ranNumGenTwo.toString(), Config.Game.CENTER_X - 190, Config.Game.CENTER_Y - 110, true);
+            stage.addChild(diceDesignTwo);
         });
     }
 
